912db17bc55bffbb0d12fba61e13e17e4e848c12
Rahul Rajagopalan <rahulrajago@gmail.com>
2011-09-11 13:18:49 -0400

    added passes arg through test

--- method_missing_test.py
+++ method_missing_test.py
@@ -34,18 +34,39 @@ class MethodMissingTest(unittest.TestCase):
 
   def test_method_missing_passes_self_through(self):
     class TestSelfPassedClass(MethodMissing):
-      def __init__(self, assertEquals):
-        self.assertEquals = assertEquals
+      def __init__(self, tester):
+        self.tester = tester
         self.self_reference = self
 
       def method_missing(self, method_name, *args, **kwargs):
-        self.assertEquals(self.self_reference, self,
+        self.tester.assertTrue(isinstance(self, TestSelfPassedClass),
+            'self should be of the right class')
+        self.tester.assertEquals(self.self_reference, self,
             'self should be passed through')
 
-    test_obj = TestSelfPassedClass(self.assertEquals)
+    test_obj = TestSelfPassedClass(self)
     test_obj.nonexistent_method()
 
   def test_method_missing_passes_args_through(self):
+    class TestArgsPassedClass(MethodMissing):
+      def __init__(self):
+        self.arg_value1 = None
+        self.arg_value2 = None
+
+      def method_missing(self, method_name, *args, **kwargs):
+        self.arg_value1 = args[0]
+        self.arg_value2 = args[1]
+
+    test_obj = TestArgsPassedClass()
+    foo_arg = 'foo'
+    bar_arg = 'bar'
+    test_obj.nonexistent_method(foo_arg, bar_arg)
+    self.assertEquals(foo_arg, test_obj.arg_value1,
+        'first positional arg should be passed through')
+    self.assertEquals(bar_arg, test_obj.arg_value2,
+        'second positional arg should be passed through')
+
+  def test_method_missing_passes_kwargs_through(self):
     pass
 
 if __name__ == '__main__':
